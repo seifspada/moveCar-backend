ff96ae5216a7f193560a124fbcae013122050528
Seif Eddine Marzougui <seifmarzougui555@gmail.com>
2026-05-15 02:41:04 +0100

    alerte activate

--- src/Module/alertes/alertes.module.ts
+++ src/Module/alertes/alertes.module.ts
@@ -1,21 +1,26 @@
 // src/Module/alertes/alertes.module.ts
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios'; // ← ajout
 import { AlertesService } from './alertes.service';
 import { AlertesResolver } from './alertes.resolver';
+import { AlertesController } from './alertes.controller';
 import { PrismaService } from '../../prisma/prisma.service';
 import { EmailService } from '../email/email.service';
 import { GeoService } from '../geo/geo.service';
 import { NotificationModule } from '../notification/notification.module';
 
 @Module({
-  imports: [NotificationModule],   // ← ici, pas dans providers
+  imports: [
+    HttpModule,         // ← ajout
+    NotificationModule,
+  ],
   providers: [
     AlertesService,
     AlertesResolver,
+    AlertesController,
     PrismaService,
     EmailService,
     GeoService,
-    // NotificationModule retiré d'ici
   ],
   exports: [AlertesService],
 })
